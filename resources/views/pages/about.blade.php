dc9bb1a250c7dd8f4aa3817d102d12330b5cae4f
shadrackongera <shadrack.sme@gmail.com>
2022-08-07 01:45:28 +0300

    about page

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -7,7 +7,19 @@
         </div>
     </div>
     <div class="py-5"></div>
-    hello spacing
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-6">
+                <img src="{{ asset('images/pics/phone-dark.png') }}" alt="..." class="img-fluid">
+            </div>
+            <div class="col-sm-6">
+                <h2>Lorem ipsum dolor sit.</h2>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad aliquid animi architecto at atque dolores eos exercitationem inventore magnam nulla numquam odio optio quam, quisquam repellat, repellendus temporibus tenetur ullam? Beatae, consequatur dolores ducimus eligendi et exercitationem fuga illum iure laboriosam magnam non officiis, possimus quibusdam sequi sint sunt vel.
+                </p>
+            </div>
+        </div>
+    </div>
     <div class="py-5"></div>
     <div class="py-5"></div>
 @endsection
